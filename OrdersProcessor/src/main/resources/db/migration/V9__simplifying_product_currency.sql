b1ad96dffb105be14f42c885e57d55d9833bd082
petruacsinte <petruacsinte@hotmail.com>
2026-03-12 03:11:34 -0400

    Fixing currency for order lines.

--- OrdersProcessor/src/main/resources/db/migration/V9__simplifying_product_currency.sql
+++ OrdersProcessor/src/main/resources/db/migration/V9__simplifying_product_currency.sql
@@ -5,6 +5,9 @@ COMMENT ON COLUMN orders.products.currency IS 'ISO 4217';
 ALTER TABLE orders.orders ADD currency varchar(3) NULL;
 COMMENT ON COLUMN orders.orders.currency IS 'ISO 4217';
 
+ALTER TABLE orders.order_lines ADD currency varchar(3) NULL;
+COMMENT ON COLUMN orders.order_lines.currency IS 'ISO 4217';
+
 -- populating current column based on existing data
 UPDATE orders.products p
 SET currency = c.currency
@@ -20,9 +23,18 @@ WHERE o.currency_id = c.id;
 
 COMMIT;
 
+UPDATE orders.order_lines ol
+SET currency = c.currency
+FROM orders.currencies c
+WHERE ol.currency_id = c.id;
+
+COMMIT;
+
+
 -- drop currencies and FK
 DROP TABLE orders.currencies CASCADE;
 
 -- drop replaced column
 ALTER TABLE orders.products DROP COLUMN currency_id;
 ALTER TABLE orders.orders DROP COLUMN currency_id;
+ALTER TABLE orders.order_lines DROP COLUMN currency_id;
